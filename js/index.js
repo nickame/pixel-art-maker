03368272500ed6064c59ef9df117faa36c865ab0
nickame <nickame13@gmail.com>
2019-11-22 02:17:08 +0300

    task 9 from 9

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
-//выборка ячеек палета
+// заполненин холста
+let container = document.getElementById('container');
+for (let i = 0; i < 20; i++) {
+  let row = document.createElement('div');
+    row.classList+='row';
+    container.appendChild(row);
+  for (let j = 0; j < 30; j++) {
+    let column = document.createElement('div');
+      column.classList+='column';
+      row.appendChild(column);
+  }
+}
+// создание палитры
+let colors = ['white','black','silver','gray','maroon', 'red', 'purple',
+             'fuchsia','green', 'lime', 'olive', 'yelloy', 'navy',
+             'blue', 'teal', 'aqua'];
+let colorItems = document.getElementById('colors');
+ for (var i = 0; i < colors.length; i++) {
+   let colorItem = document.createElement('li');
+   colorItem.classList+='colorItem';
+   colorItem.style.backgroundColor= colors[i];
+   colorItems.appendChild(colorItem);
+ }
+
+//выборка ячеек холста
 let allCell = document.getElementsByClassName('column');
 let cell = Array.prototype.filter.call(allCell, function(allCelll){
   return allCelll.nodeName === 'DIV';
@@ -11,14 +35,18 @@ expColor.style.backgroundColor= color;
 //выборцвета
 let chColor = document.getElementById('colors');
 chColor.addEventListener("click", function(event) {
-  console.log(color);
   color=window.getComputedStyle(event.target).backgroundColor;
   expColor.style.backgroundColor= color;
-  console.log(color);
 })
-// обработчик событий для палета
+// обработчик событий для холста
 for (var i = 0; i < cell.length; i++) {
   cell[i].addEventListener("click", function(event){
+     event.currentTarget.style.backgroundColor= color;
+   });
+}
+for (var i = 0; i < cell.length; i++) {
+  cell[i].addEventListener("mousemove", function(event){
+    if(event.buttons==1)
      event.currentTarget.style.backgroundColor= color;
    });
 }
