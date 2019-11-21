10059602a25ca7c55c0a82b08907b3f854773207
nickame <nickame13@gmail.com>
2019-11-21 23:43:29 +0300

    task 3 from 9

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
- 
+let allCell = document.getElementsByClassName('column');
+let cell = Array.prototype.filter.call(allCell, function(allCelll){
+  return allCelll.nodeName === 'DIV';
+});
+let color='red';
+for (var i = 0; i < cell.length; i++) {
+  cell[i].addEventListener("click", function(event){
+     event.currentTarget.style.backgroundColor= color;
+   });
+}
+
+  // cel.addEventListener("click", function(event){
+  //   event.currentTarget.style.backgroundColor='red';
+  // });
+
+// function chengeBackground(event) {
+// event.currentTarget.style.backgroundColor='red';
+// }
