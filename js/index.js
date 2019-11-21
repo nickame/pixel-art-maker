13d525f5a882079778ab2636f66598a6af3d6e9b
nickame <nickame13@gmail.com>
2019-11-22 00:51:43 +0300

    task 5 from 9

--- js/index.js
+++ js/index.js
@@ -1,18 +1,24 @@
+//выборка ячеек палета
 let allCell = document.getElementsByClassName('column');
 let cell = Array.prototype.filter.call(allCell, function(allCelll){
   return allCelll.nodeName === 'DIV';
 });
+// цвет образца
 let color='red';
+let expColor = document.getElementById('colorExample');
+expColor.style.backgroundColor= color;
+
+//выборцвета
+let chColor = document.getElementById('colors');
+chColor.addEventListener("click", function(event) {
+  console.log(color);
+  color=window.getComputedStyle(event.target).backgroundColor;
+  expColor.style.backgroundColor= color;
+  console.log(color);
+})
+// обработчик событий для палета
 for (var i = 0; i < cell.length; i++) {
   cell[i].addEventListener("click", function(event){
      event.currentTarget.style.backgroundColor= color;
    });
 }
-
-  // cel.addEventListener("click", function(event){
-  //   event.currentTarget.style.backgroundColor='red';
-  // });
-
-// function chengeBackground(event) {
-// event.currentTarget.style.backgroundColor='red';
-// }
